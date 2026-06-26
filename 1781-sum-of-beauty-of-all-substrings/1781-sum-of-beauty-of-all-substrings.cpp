68933c42135b5ac278fc602d3ec9ac9f58446531
Sandesh Gupta <sandeshgupta3867@gmail.com>
2026-06-26 05:46:36 +0530

    Time: 67 ms (86.07%), Space: 11.1 MB (78.1%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -5,21 +5,23 @@ public:
         int sum = 0;
 
         for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                vector<int> v(26, 0);
-                for (int k = i; k <= j; k++) {
-                    v[s[k] - 'a']++;
-                }
+            vector<int> v(26, 0);
+            v[s[i] - 'a']++;
+
+            for (int j = i + 1; j < n; j++) {
+                v[s[j] - 'a']++;
+
                 int mx = INT_MIN, mn = INT_MAX;
                 for (auto x : v) {
-                    if (x!=0 && x > mx) {
+                    if (x != 0 && x > mx) {
                         mx = x;
                     }
-                    if (x!=0 && x < mn) {
+                    if (x != 0 && x < mn) {
                         mn = x;
                     }
                 }
-                if(mn==INT_MAX || mx==INT_MIN) continue;
+
+                if (mn == INT_MAX || mx == INT_MIN)   continue;
                 sum += (mx - mn);
             }
         }
