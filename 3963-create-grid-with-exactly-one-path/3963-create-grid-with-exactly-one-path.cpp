c684054a576f6e4e4d968d362072fe0bdca8f475
Sandesh Gupta <sandeshgupta3867@gmail.com>
2026-07-12 22:32:01 +0530

    Time: 3 ms (35.19%), Space: 11.1 MB (75.4%) - LeetHub

--- 3963-create-grid-with-exactly-one-path/3963-create-grid-with-exactly-one-path.cpp
+++ 3963-create-grid-with-exactly-one-path/3963-create-grid-with-exactly-one-path.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     vector<string> createGrid(int m, int n) {
-        vector<string>ans;
-        vector<vector<char>>v(m,vector<char>(n,'#'));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(j==0 || i==m-1){
-                    v[i][j]='.';
-                }
-            }
-        }
-        for(int i=0;i<m;i++){
-            string s;
-            for(int j=0;j<n;j++){
-                s+=v[i][j];
-            }
-            ans.push_back(s);
-        }
+        vector<string>ans(m,string(n,'#'));
+
+        for(int i=0;i<m;i++)    ans[i][0]='.';
+        for(int j=0;j<n;j++)    ans[m-1][j]='.';
+        
         return ans;
     }
 };
